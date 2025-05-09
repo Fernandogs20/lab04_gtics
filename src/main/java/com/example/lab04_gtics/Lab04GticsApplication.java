47dc00dcb654056fc867cdc29879853a428a5da6
Fernandogs20 <a20206311@pucp.edu.pe>
2025-05-08 20:47:05 -0500

    Hola

---
+++ src/main/java/com/example/lab04_gtics/Lab04GticsApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab04_gtics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab04GticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab04GticsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab04_gtics/Lab04GticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab04_gtics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab04GticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
